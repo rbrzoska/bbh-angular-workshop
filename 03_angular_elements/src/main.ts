394696811b382c89c416f0807669b8b41af9e937
Rafał Brzoska <rafal@brzoska.net.pl>
2018-03-08 06:50:52 +0100

    Added angular elements

---
+++ 03_angular_elements/src/main.ts
@@ -0,0 +1,15 @@
+import './polyfills';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { registerAsCustomElements } from '../@angular/elements';
+
+import { AppModule, customElements } from './app/app.module';
+
+registerAsCustomElements(customElements, () => {
+    return platformBrowserDynamic().bootstrapModule(AppModule);
+})
+    .then(() => {
+        // App is bootstrapped
+    }, (error) => {
+        // There's a bootstrapping error
+    });
